297489fea25150364b7cee5cdeb3668475356c17
liamLacuna <lon.liam.li@gmail.com>
2021-12-01 19:54:18 -0800

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Liam Li on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
