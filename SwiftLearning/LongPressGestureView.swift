8d5c0bd7b5c394058b5ecd3ecdec91befe4a7365
liamLacuna <lon.liam.li@gmail.com>
2021-12-05 16:54:11 -0800

    learn longPressGestureView

---
+++ SwiftLearning/LongPressGestureView.swift
@@ -0,0 +1,81 @@
+//
+//  LongPressGestureView.swift
+//  SwiftLearning
+//
+//  Created by Liam Li on 12/1/21.
+//
+
+import SwiftUI
+
+struct LongPressGestureView: View {
+    
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool = false
+    
+    var body: some View {
+        
+        VStack {
+            Rectangle()
+                .fill(isSuccess ? Color.green: Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("CLICK HERE")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) { (isPressing) in
+                        // start of press -> min duration
+                        if isPressing {
+                            withAnimation(.easeInOut(duration: 1.0)) {
+                                isComplete = true
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess {
+                                    withAnimation(.easeOut) {
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    } perform: {
+                        withAnimation(.easeOut) {
+                            isSuccess = true
+                        }
+                    }
+                
+                Text("RESET")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+        
+//        Text(isComplete ? "COMPLETED" : "NOT COMPLETE")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+//            .onLongPressGesture(
+//                minimumDuration: 5.0, maximumDistance: 50
+//            ) {
+//                isComplete.toggle()
+//            }
+    }
+}
+
+struct LongPressGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGestureView()
+    }
+}
